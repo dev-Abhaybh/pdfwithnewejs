34fd38114c648f7b3c0d3387dc0f047b1a41eb12
abhay <abhaybh@vigorushealth.com>
2023-03-29 15:00:27 +0530

    dir info

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const PORT = 8004;
 app.listen(PORT, (error) => {
   if (!error)
     console.log(
-      "Server is Successfully Running, and App is listening on port " + PORT
+      "Server is Successfully Running, and App is listening on port " + PORT,
+      __dirname
     );
   else console.log("Error occurred, server can't start", error);
 });
