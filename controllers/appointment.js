f3a72b15b666263bb5b45c495a45b1e2907be179
abhay <abhaybh@vigorushealth.com>
2023-04-18 10:13:07 +0530

    with rx templete

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -5,6 +5,9 @@ const ejs = require("ejs");
 require("dotenv").config();
 module.exports.pdfGenerator = async (req, res) => {
   const user = req.body;
+  console.log("jijfdlsfisd");
+  console.log(user);
+  const details = req.body;
 
   const compile = async (template, data) => {
     const filePath = path.join(process.cwd(), "views", `${template}.ejs`);
@@ -25,7 +28,7 @@ module.exports.pdfGenerator = async (req, res) => {
       console.log("in generate pdf function now");
 
       const page = await browseer.newPage();
-      const content = await compile("abc", { user });
+      const content = await compile("rx", { data: details });
       await page.setContent(content);
       await page.emulateMediaFeatures("screen");
       console.log("before pdf");
@@ -38,6 +41,7 @@ module.exports.pdfGenerator = async (req, res) => {
       if (pdf) {
         let file = await fs.createReadStream(process.cwd() + "/tmp/abc.pdf");
         let stat = fs.statSync(process.cwd() + "/tmp/abc.pdf");
+
         return { file, stat, pdf };
       }
       console.log(pdf);
@@ -47,19 +51,18 @@ module.exports.pdfGenerator = async (req, res) => {
       await browseer.close();
     }
   };
-  const data = await createPdf();
+  const data1 = await createPdf();
   // const data = await fs.readFileSync(process.cwd() + "/tmp/abc.pdf");
   // res.contentType("application/pdf");
-
   // res.send(data);
   //console.log(data);
-  const file = data.pdf;
-
-  res.setHeader("Content-Length", data.stat.size);
+  const file = data1.pdf;
+  console.log(file);
+  res.setHeader("Content-Length", data1.stat.size);
   res.setHeader("Content-Type", "application/pdf");
   res.setHeader("Content-Disposition", "attachment; filename=quote.pdf");
   //console.log(file.pipe(res));
-  // file.pipe(res);
+  //file.pipe(res);
   return res.send(file);
   // res.setHeader("Content-Type", "application/pdf").send(pdf);
   // res.send(pdf);
