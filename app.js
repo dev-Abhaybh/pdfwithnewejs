80f922533e291b532b5818de52fda87a7c7b150a
abhay <abhaybh@vigorushealth.com>
2023-04-26 15:16:49 +0530

    no db

--- app.js
+++ app.js
@@ -22,37 +22,4 @@ app.set("views", path.join(__dirname, "/views"));
 
 app.use("/appointment", appointmentRoute);
 
-// // connecting mongoose with mongodb
-
-// mongoose.set("strictQuery", true);
-
-// mongoose.connect(
-//   "mongodb+srv://Abhay:abhay123@cluster0.2hu46of.mongodb.net/?retryWrites=true&w=majority"
-//   //mongodb+srv://abhay:abhay123@cluster0.tpuujrd.mongodb.net/?retryWrites=true&w=majority
-//   // useNewUrlParser: true,
-//   // useUnifiedTopology: true,
-//   // useCreateIndex: true,
-//   // useFindAndModify: false
-// );
-
-// //checking for conformation of connectivity for database
-
-// const db = mongoose.connection;
-// db.on(
-//   "error",
-//   console.error.bind(
-//     console,
-//     "connection error:****************************************************"
-//   )
-// );
-// db.once("open", () => {
-//   console.log("database connected!!");
-// });
-
-// module.exports = db;
-
-// const schema = graphQl.graphQl();
-
-// app.use("/graphql", expressGraphQL({ schema: schema, graphiql: true }));
-
 module.exports = app;
